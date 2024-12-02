3209bb74c4894788d5ddf88f66f3f3ab29359368
Evgeny <evgenygorokhovg@yahoo.com>
2024-12-02 21:25:15 +0300

    Structured the script

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from dotenv import load_dotenv
 from pytimeparse import parse
 
 
-load_dotenv()
-BOT = ptbot.Bot(os.getenv('TG_TOKEN'))
-
-
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
     percent = "{0:.1f}"
@@ -17,27 +13,29 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def notify(chat_id):
-    BOT.send_message(chat_id, 'Время вышло')
+def notify(chat_id, bot):
+    bot.send_message(chat_id, 'Время вышло')
 
 
-def notify_progress(secs_left, chat_id, message_id, timer_time):
+def notify_progress(secs_left, chat_id, message_id, timer_time, bot):
     progress_message = f'Осталось {secs_left} секунд\n' + render_progressbar(timer_time, timer_time - secs_left)
-    BOT.update_message(chat_id, message_id, progress_message)
+    bot.update_message(chat_id, message_id, progress_message)
 
 
-def reply(chat_id, user_message):
-    message_id = BOT.send_message(chat_id, f'Запускаю таймер:')
-    BOT.create_countdown(
+def reply(chat_id, user_message, bot):
+    message_id = bot.send_message(chat_id, f'Запускаю таймер:')
+    bot.create_countdown(
         parse(user_message), notify_progress, chat_id=chat_id,
-        message_id=message_id, timer_time=parse(user_message)
+        message_id=message_id, timer_time=parse(user_message), bot=bot
     )
-    BOT.create_timer(parse(user_message), notify, chat_id=chat_id)
+    bot.create_timer(parse(user_message), notify, chat_id=chat_id, bot=bot)
 
 
 def main():
-    BOT.reply_on_message(reply)
-    BOT.run_bot()
+    load_dotenv()
+    bot = ptbot.Bot(os.getenv('TG_TOKEN'))
+    bot.reply_on_message(reply, bot=bot)
+    bot.run_bot()
 
 
 if __name__ == '__main__':
